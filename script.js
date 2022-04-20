748f0eb36db6f280a0706d4f1b0d7f8ffb0a5303
= <najeebullah.joya79@gmail.com>
2022-04-20 22:24:16 +0430

    access the important elements

---
+++ script.js
@@ -0,0 +1,6 @@
+const timer_lable = document.querySelector('.timer');
+const stopwath_lable = document.querySelector('.stop-watch');
+const hour = document.querySelector('.hour');
+const minute = document.querySelector('.minute');
+const second = document.querySelector('.second');
+const btn_start = document.querySelector('.start');
